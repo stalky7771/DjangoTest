099e0d5e247189b5e0505b927fc315b320447901
stalky <stalky7771@protonmail.com>
2024-05-01 15:48:56 +0300

    work with templates

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Course
 
 def index(request):
-    return HttpResponse("Hello from the Shop app AAA")
+    courses = Course.objects.all()    
+    return render(request, 'courses.html', {'courses': courses})
